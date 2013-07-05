a332257f2ad81db7944249630f852ec062385625
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2013-07-05 17:44:23 +0100

    Almost completed tests

--- socket2/test/sockjstest.js
+++ socket2/test/sockjstest.js
@@ -2,10 +2,12 @@ var tap = require("tap"),
     resource = require('resource'),
     io = require('sockjs-client');
 
-tap.test('create a socket server with the sockjs engine', function (t) {
-  require('../index.js'); //Instead of including the resource with .use I require it as it isn't an npm module yet.
+
   resource.use('http');
   resource.use('creature');
+  require('../index.js'); //Instead of including the resource with .use I require it as it isn't an npm module yet.
+
+tap.test('create a socket server with the sockjs engine', function (t) {
   resource.http.listen(function (err, server) {
     t.error(err, 'http resource listened');
     resource.socket.start({engine: 'sock.js'}, function (err) {
@@ -20,29 +22,34 @@ tap.test('connect to socket server', function (t) {
   t.doesNotThrow(function () {
     client = io.create('http://localhost:8888');
   }, 'client created successfully');
+
   t.type(client, 'object', 'client is defined');
+
   client.on('connection', function () {
     t.pass('client connected');
-    t.end();
+    t.end()
   });
 });
 
 tap.test('create a creature', function (t) {
   t.doesNotThrow(function(){
-    client.emit('creature', 'create', { id: 'korben' });
-  })
+    var json = JSON.stringify(['creature', 'create', { id: 'korben' }]);
+    client.write(json);
+  });
+
   resource.creature.on('create', function(){
     t.pass('creature created');
+    t.end();
   });
 });
 
 //tap.test('get created creature', function (t) {
-  client.emit('creature', 'get', 'korben', function (err, result) {
-    t.error(err, 'no error');
-    t.type(result, 'object', 'creature is an object');
-    t.equal(result.id, 'korben', 'id is korben');
-    t.end();
-  });
+//  client.emit('creature', 'get', 'korben', function (err, result) {
+//    t.error(err, 'no error');
+//    t.type(result, 'object', 'creature is an object');
+//    t.equal(result.id, 'korben', 'id is korben');
+//    t.end();
+//  });
 //});
 //
 //tap.test('all creatures', function (t) {
